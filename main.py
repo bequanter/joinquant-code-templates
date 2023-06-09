8f57aafc1bc9a586379e73f5b3c61b59fa646483
Benwei Zhu <benweizhu@gmail.com>
2023-06-09 15:22:57 +0800

    初始化模版

--- main.py
+++ main.py
@@ -22,6 +22,92 @@ def initialize(context):
     run_daily(after_market_close, time='after_close', reference_security='000300.XSHG')
 
 
+## 开盘前运行函数
+def before_market_open(context):
+    # 根据策略选出股票池
+    g.stock_pool = get_stocks(context)
+
+
+def trading_now(context):
+    return True
+
+
+def should_buy(context):
+    return True
+
+
+def should_sell(context):
+    return True
+
+
+def should_hold(context):
+    return True
+
+
+## 开盘时运行函数
+def market_open(context):
+    long_list = []
+    short_list = []
+    hold_list = []
+
+    if (trading_now(context) == True):
+        for stock in g.stock_pool:
+            if (should_buy(context) == True):
+                long_list.append(stock)
+            if (should_sell(context) == True):
+                short_list.append(stock)
+    current_stock_set = list(context.portfolio.positions.keys())
+
+    for stock in current_stock_set:
+        if (stock in short_list):
+            order_target_value(stock, 0)
+        else:
+            hold_list.append(stock)
+    buy_list = []
+    for stock in long_list:
+        if stock not in hold_list:
+            buy_list.append(stock)
+
+    if len(buy_list) == 0:
+        Cash = context.portfolio.available_cash;
+    else:
+        Cash = context.portfolio.available_cash / len(buy_list)
+        for stock in buy_list:
+            order_target_value(stock, Cash)
+
+
+## 收盘后运行函数
+def after_market_close(context):
+    # 得到当天所有成交记录
+    trades = get_trades()
+    for _trade in trades.values():
+        log.info('成交记录：' + str(_trade))
+
+
+def get_stocks(context):
+    selected_stocks = get_fundamentals(
+        query(valuation.code,
+              valuation.pe_ratio,
+              valuation.pb_ratio,
+              valuation.market_cap,
+              indicator.eps,
+              indicator.inc_net_profit_annual)
+        .filter(valuation.pe_ratio < 40,
+                valuation.pe_ratio > 10,
+                indicator.eps > 0.3,
+                indicator.inc_net_profit_annual > 0.30,
+                indicator.roe > 15)
+        .order_by(valuation.pb_ratio.asc())
+        .limit(50),
+        date=None)
+
+    stock_pool = list(selected_stocks['code'])
+    stock_pool = paused_filter(stock_pool)
+    stock_pool = delisted_filter(stock_pool)
+    stock_pool = st_filter(stock_pool)
+    return stock_pool
+
+
 def set_params(context):
     # 设定起始日期为0
     g.days = 0
@@ -35,11 +121,6 @@ def set_backtest(context):
 
     # 开启动态复权模式(真实价格)
     set_option('use_real_price', True)
-
-    # 输出内容到日志 log.info()
-    log.info('初始函数开始运行且全局只运行一次')
-    # 过滤掉order系列API产生的比error级别低的log
-    # log.set_level('order', 'error')
 
 
 def set_slip_fee(context):
@@ -68,82 +149,3 @@ def st_filter(security_list):
     current_data = get_current_data()
     security_list = [stock for stock in security_list if not current_data[stock].is_st]
     return security_list
-
-
-def get_stocks(context):
-    selected_stocks = get_fundamentals(
-        query(valuation.code,
-              valuation.pe_ratio,
-              valuation.pb_ratio,
-              valuation.market_cap,
-              indicator.eps,
-              indicator.inc_net_profit_annual)
-        .filter(valuation.pe_ratio < 40,
-                valuation.pe_ratio > 10,
-                indicator.eps > 0.3,
-                indicator.inc_net_profit_annual > 0.30,
-                indicator.roe > 15)
-        .order_by(valuation.pb_ratio.asc())
-        .limit(50),
-        date=None)
-
-    stock_pool = list(selected_stocks['code'])
-    stock_pool = paused_filter(stock_pool)
-    stock_pool = delisted_filter(stock_pool)
-    stock_pool = st_filter(stock_pool)
-    return stock_pool
-
-
-## 开盘前运行函数
-def before_market_open(context):
-    # 输出运行时间
-    log.info('函数运行时间(before_market_open)：' + str(context.current_dt.time()))
-
-    # 给微信发送消息（添加模拟交易，并绑定微信生效）
-    # send_message('美好的一天~')
-
-    # 根据策略选出股票池
-    stock_pool = get_stocks(context)
-
-    if (len(stock_pool) > 0):
-        g.security = stock_pool[0]
-    else:
-        g.security = '000001.XSHE'
-
-
-## 开盘时运行函数
-def market_open(context):
-    log.info('函数运行时间(market_open):' + str(context.current_dt.time()))
-    security = g.security
-    # 获取股票的收盘价
-    close_data = get_bars(security, count=5, unit='1d', fields=['close'])
-    # 取得过去五天的平均价格
-    MA5 = close_data['close'].mean()
-    # 取得上一时间点价格
-    current_price = close_data['close'][-1]
-    # 取得当前的现金
-    cash = context.portfolio.available_cash
-
-    # 如果上一时间点价格高出五天平均价1%, 则全仓买入
-    if (current_price > 1.01 * MA5) and (cash > 0):
-        # 记录这次买入
-        log.info("价格高于均价 1%%, 买入 %s" % (security))
-        # 用所有 cash 买入股票
-        order_value(security, cash)
-    # 如果上一时间点价格低于五天平均价, 则空仓卖出
-    elif current_price < MA5 and context.portfolio.positions[security].closeable_amount > 0:
-        # 记录这次卖出
-        log.info("价格低于均价, 卖出 %s" % (security))
-        # 卖出所有股票,使这只股票的最终持有量为0
-        order_target(security, 0)
-
-
-## 收盘后运行函数
-def after_market_close(context):
-    log.info(str('函数运行时间(after_market_close):' + str(context.current_dt.time())))
-    # 得到当天所有成交记录
-    trades = get_trades()
-    for _trade in trades.values():
-        log.info('成交记录：' + str(_trade))
-    log.info('一天结束')
-    log.info('##############################################################')
